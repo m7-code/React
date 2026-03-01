a73deb903239ddf8b14a3a182ffd1eb49af0632d
m7-code <mughiraasad6@gmail.com>
2026-03-01 02:43:22 -0800

    adding my reat work

---
+++ 05_module_css/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './components/button/button'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+        <Button />
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ 05_module_css/src/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './button.module.css'
+
+const Button = () => {
+  return (
+    <div className={style.btn}>button</div>
+  )
+}
+
+export default Button
